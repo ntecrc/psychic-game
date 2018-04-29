373c86e5ec0ded601d7f69506eb319fd83425fe2
unknown <gonzalezrc@gmail.com>
2018-04-29 11:45:34 -0400

    finished...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,14 +21,71 @@
 // wins otherwise you lose. For debugging purposes I used a return (also not sure if I should do that... 
 // seemed appropriate.) I then called the function and input the user's choice -- in this
 // case the user chose the letter n.
-function userGuess() {
-    var compRanPick = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-    if (userGuess === compRanPick) {
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var wins = 0;
+var losses = 0;
+
+
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+    var compRanPick = letters[Math.floor(Math.random() * letters.length)];
+    if (userGuess === compRanPick && turns < 5) {
+        new Audio("assets/sounds/winner.mp3").play();
+        wins++;
+        
+        alert("Great, you guess right!");
+        
         console.log('You win!');
-    } else {
+        console.log('my choice was ' + userGuess );
+        console.log('Computer choice ' + compRanPick);
+    } else if (userGuess !== compRanPick){
+        new Audio("assets/sounds/loser.mp3").play();
+        losses++;
+        
+        alert("Sorry you didnt pick the right letter.");
         console.log('You lose!');
-    }
-    return this.compRanPick
-};
-userGuess('n');    
+        console.log('User choice was ' + userGuess );
+        console.log('Computer choice ' + compRanPick);
+        } 
+        var html =
+        "<p>You chose: " + userGuess + "</p>" +
+        "<p>The computer chose: " + compRanPick + "</p>" +
+        "<p>wins: " + wins + "</p>" +
+        "<p>losses: " + losses + "</p>";
+        
+        document.querySelector("#display").innerHTML = html;
+    };
+       
+    
+        
+ 
+
+
+      
+            
+      
+            
+      
+            
+      
+            
+      
+//// Be sure to test that your code works for this major task, before proceeding to the next one!
+      
+//// MAJOR TASK #3: ATTACH ON-CLICK EVENTS TO "CLEAR" BUTTON
+//// =================================================================================
+      
+//// 12. Create an "on-click" event attached to the "#clear" button id.
+//$("#clear").click(function() {
+//    // $(this).slideUp(); this is a event test
+//    $("#display").empty();
+            
+//  });
+//// Inside the on-click event...
+      
+//// 13. Use the jQuery "empty()" method to clear the contents of the "#display" div.
+      
+//;
+        
 
